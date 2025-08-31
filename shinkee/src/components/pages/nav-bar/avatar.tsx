99b7b2cd7a4aa6ac5bc16e835f309a03119bda0f
Shinkee Cho <development@magzog.com>
2025-08-31 18:38:01 +1200

    Use next/image instead of img.

--- shinkee/src/components/pages/nav-bar/avatar.tsx
+++ shinkee/src/components/pages/nav-bar/avatar.tsx
@@ -1,4 +1,5 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react"
+import Image from "next/image"
 
 interface AvatarProps {
   navigation: { 
@@ -35,8 +36,10 @@ export function Avatar({ navigation }: AvatarProps) {
 
 export function AvatarRoundImage() {
   return (
-    <img
+    <Image
       alt=""
+      width={256}
+      height={256}
       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
       className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
     />
